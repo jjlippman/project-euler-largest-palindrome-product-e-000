983245b8177ed802edcfab88f28bb37e803ee241
jjlippman <jjlippman@earthlink.net>
2016-01-11 13:04:03 -0700

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,32 @@
 # Implement your procedural solution here!
+
+
+  def largest_palindrome_product
+    pal_array =[]
+    first = 999
+    
+   while first>=111 do
+     second = 999
+     while second >=111 do
+      num = first * second
+      
+        if isPalindrome?(num)
+         
+          pal_array << num
+        end
+      second-=1
+     end
+     first-=1
+    end
+
+ return pal_array.max
+ end
+
+def isPalindrome?(num)
+  num = num.to_s
+  if num.reverse == num
+    return true
+  else
+    return false
+  end
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,39 @@
 # Implement your object-oriented solution here!
+
+class LargestPalindromeProduct
+
+ def answer
+    pal_array =[]
+    first = 999
+    
+   while first>=111 do
+     second = 999
+     while second >=111 do
+      num = first * second
+      
+        if isPalindrome?(num)
+         
+          pal_array << num
+        end
+      second-=1
+     end
+     first-=1
+    end
+
+ return pal_array.max
+ end
+
+  def isPalindrome?(num)
+        num = num.to_s
+        if num.reverse == num
+          return true
+        else
+          return false
+        end
+   end
+
+  
+
+
+
+end
